7fba534cbd5b498ba235c40bfde7009ae3152196
lixurui <lixurui@appledeMacBook-Pro-10.local>
2019-09-03 22:46:07 +0800

    init

---
+++ input/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import itchat, time
+from itchat.content import *
+
+@itchat.msg_register([PICTURE, RECORDING, ATTACHMENT, VIDEO])
+def download_files(msg):
+    msg.download(msg.fileName)
+    typeSymbol = {
+        PICTURE: 'img',
+        VIDEO: 'vid', }.get(msg.type, 'fil')
+    return '@%s@%s' % (typeSymbol, msg.fileName)
+
+    # itchat.send_msg(m, msg.fromUserName)
+
+
+# itchat.auto_login(enableCmdQR=0, hotReload=True) # 如果是在window上或者有可视化界面的ubuntu系统上，可以用这个
+itchat.auto_login(enableCmdQR=0, hotReload=True)  # enableCmdQR=1是在控制台输出微信登录二维码
+itchat.run()
---
+++ plot/main.py
@@ -0,0 +1,25 @@
+from pydub import AudioSegment
+import matplotlib.pyplot as plt
+import wave
+import numpy as np
+
+def trans_mp3_to_wav(filepath):
+    song = AudioSegment.from_mp3(filepath)
+    # print(song.raw_data)
+    # song.export("new.wav",format="wav")
+
+
+if __name__ == "__main__":
+    # trans_mp3_to_wav("./data/abc.mp3")
+    f = wave.open("./new.wav")
+    nchannels, width, frame_rate, frame_n = f.getparams()[:4]
+    print(nchannels)
+    print(frame_n)
+    time = np.arange(0, frame_n) * (1.0/frame_rate)
+    strData = f.readframes(frame_n)
+    waveData = np.frombuffer(strData,np.short)
+    waveData = waveData * 1.0 / np.max(np.abs(waveData))
+    print(time.shape)
+    print(waveData.shape)
+    plt.plot(time, waveData)
+    plt.show()
